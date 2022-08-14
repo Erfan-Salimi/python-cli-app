d6a6738f49381527a6f9c1bd4aa22e700cf8a85c
Erfan <erfan.ali.salimi@gmail.com>
2022-08-14 22:07:43 +0430

    add program options

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("start")
+from pyfiglet import Figlet
+from colorama import Fore, init
+init()
+
+f = Figlet(font='doom')
+print(Fore.RED + f.renderText('CLI python'))
+
+print(Fore.RED + "1." + Fore.CYAN + "Weather", end="\t")
+print(Fore.RED + "2." + Fore.CYAN + "Calender", end="\t")
+print(Fore.RED + "3." + Fore.CYAN + "Coins", end="\n")
+
+print(Fore.RED + "4." + Fore.CYAN + "Screen shot", end="\t")
+print(Fore.RED + "5." + Fore.CYAN + "Screen recorder", end="\t")
+print(Fore.RED + "6." + Fore.CYAN + "WhatsApp message", end="\n")
+
+print(Fore.RED + "7." + Fore.CYAN + "Email", end="\t")
+print(Fore.RED + "8." + Fore.CYAN + "Calculator", end="\t")
+print(Fore.RED + "9." + Fore.CYAN + "Ip address", end="\n")
+
+print(Fore.RED + "10." + Fore.CYAN + "Alarm", end="\t")
+print(Fore.RED + "11." + Fore.CYAN + "Clock", end="\t")
+print(Fore.RED + "12." + Fore.CYAN + "Corona statistics", end="\n")
+
+print(Fore.RED + "13." + Fore.CYAN + "Changing the base of numbers", end="\t")
+print(Fore.RED + "14." + Fore.CYAN + "Text to morse code", end="\t")
+print(Fore.RED + "15." + Fore.CYAN + "convert images to pdf", end="\n")
+
+print(Fore.RED + "16." + Fore.CYAN + "Random number", end="\t")
+print(Fore.RED + "17." + Fore.CYAN + "Text to speech", end="\t")
+print(Fore.RED + "18." + Fore.CYAN + "Check internet connection", end="\n")
+
+print(Fore.RED + "19." + Fore.CYAN + "Hangman", end="\t")
+print(Fore.RED + "20." + Fore.CYAN + "Notes", end="\t")
+print(Fore.RED + "21." + Fore.CYAN + "Convert to zip file", end="\n")
+
+print(Fore.RED + "22." + Fore.CYAN + "Calculate age", end="\t")
+print(Fore.RED + "23." + Fore.CYAN + "Password generator", end="\t")
+print(Fore.RED + "24." + Fore.CYAN + "Tic Tac Toe", end="\n")
+
+print(Fore.RED + "22." + Fore.CYAN + "BMI", end="\t")
+print(Fore.RED + "23." + Fore.CYAN + "Image to ascii art", end="\t")
+print(Fore.RED + "24." + Fore.CYAN + "Reading the text", end="\n")
