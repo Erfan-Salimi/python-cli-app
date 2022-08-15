8d27ae59db61510f175aac6c5cb51c899b4b349f
Erfan <erfan.ali.salimi@gmail.com>
2022-08-15 21:48:20 +0430

    change output format

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
 from pyfiglet import Figlet
 from colorama import Fore, init
+import os, sys
+
+
+os.system("cls")
+os.chdir(sys.path[0])
 init()
 
 f = Figlet(font='doom')
-print(Fore.RED + f.renderText('python apps'))
+print(Fore.GREEN + f.renderText('python apps'))
 
-print(f'{Fore.RED}1. {Fore.CYAN}Weather{"": <{10}}', end="\t")
-print(f'{Fore.RED}2. {Fore.CYAN}Calender{"": <{10}}', end="\t")
-print(f'{Fore.RED}3. {Fore.CYAN}Coins', end="\n")
-
-print(f'{Fore.RED}4. {Fore.CYAN}Screenshot{"": <{10}}', end="\t")
-print(f'{Fore.RED}5. {Fore.CYAN}Screen recorder{"": <{6}}', end="")
-print(f'{Fore.RED}6. {Fore.CYAN}Whatsapp message', end="\n")
-
-print(f'{Fore.RED}7. {Fore.CYAN}Email{"": <{10}}', end="\t")
-print(f'{Fore.RED}8. {Fore.CYAN}Calculator{"": <{10}}', end="\t")
-print(f'{Fore.RED}9. {Fore.CYAN}Ip address', end="\n")
-
-print(f'{Fore.RED}10. {Fore.CYAN}Alarm{"": <{10}}', end="\t")
-print(f'{Fore.RED}11. {Fore.CYAN}Clock{"": <{10}}', end="\t")
-print(f'{Fore.RED}12. {Fore.CYAN}Corona statistics', end="\n")
-
-print(f'{Fore.RED}13. {Fore.CYAN}Base converter{"": <{1}}', end="\t")
-print(f'{Fore.RED}14. {Fore.CYAN}Text to morse code{"": <{0}}', end="\t")
-print(f'{Fore.RED}15. {Fore.CYAN}Convert images to pdf', end="\n")
-
-print(f'{Fore.RED}16. {Fore.CYAN}Random number{"": <{2}}', end="\t")
-print(f'{Fore.RED}17. {Fore.CYAN}Text to speech{"": <{2}}', end="\t")
-print(f'{Fore.RED}18. {Fore.CYAN}Internet connection', end="\n")
-
-print(f'{Fore.RED}19. {Fore.CYAN}Hangman{"": <{10}}', end="\t")
-print(f'{Fore.RED}20. {Fore.CYAN}Notes{"": <{10}}', end="\t")
-print(f'{Fore.RED}21. {Fore.CYAN}Convert to zip file', end="\n")
-
-print(f'{Fore.RED}22. {Fore.CYAN}Calculate age{"": <{1}}', end="\t")
-print(f'{Fore.RED}23. {Fore.CYAN}Password generator{"": <{0}}', end="\t")
-print(f'{Fore.RED}24. {Fore.CYAN}Tic Tac Toe', end="\n")
-
-print(f'{Fore.RED}25. {Fore.CYAN}BMI{"": <{10}}', end="\t")
-print(f'{Fore.RED}26. {Fore.CYAN}Image to ascii{"": <{0}}', end="\t")
-print(f'{Fore.RED}24. {Fore.CYAN}Read text', end="\n")
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}1. {Fore.WHITE}Weather', f'{Fore.GREEN}2. {Fore.WHITE}Calender', f'{Fore.GREEN}3. {Fore.WHITE}Coins'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}4. {Fore.WHITE}Screenshot', f'{Fore.GREEN}5. {Fore.WHITE}Screen recorder', f'{Fore.GREEN}6. {Fore.WHITE}Whatsapp message'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}7. {Fore.WHITE}Email', f'{Fore.GREEN}8. {Fore.WHITE}Calculator', f'{Fore.GREEN}9. {Fore.WHITE}Ip address'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}10. {Fore.WHITE}Alarm', f'{Fore.GREEN}11. {Fore.WHITE}Clock', f'{Fore.GREEN}12. {Fore.WHITE}Corona statistics'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}13. {Fore.WHITE}Base converter', f'{Fore.GREEN}14. {Fore.WHITE}Text to morse code', f'{Fore.GREEN}15. {Fore.WHITE}Convert images to pdf'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}16. {Fore.WHITE}Random number', f'{Fore.GREEN}17. {Fore.WHITE}Text to speech', f'{Fore.GREEN}18. {Fore.WHITE}Internet connection'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}19. {Fore.WHITE}Hangman', f'{Fore.GREEN}20. {Fore.WHITE}Notes', f'{Fore.GREEN}21. {Fore.WHITE}Convert to zip file'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}22. {Fore.WHITE}Calculate age', f'{Fore.GREEN}23. {Fore.WHITE}Password generator', f'{Fore.GREEN}24. {Fore.WHITE}Tic Tac Toe'))
+print("%-33s %-33s %-33s" % (f'{Fore.GREEN}25. {Fore.WHITE}BMI', f'{Fore.GREEN}26. {Fore.WHITE}Image to ascii', f'{Fore.GREEN}27. {Fore.WHITE}Read text'))
 print()
+
